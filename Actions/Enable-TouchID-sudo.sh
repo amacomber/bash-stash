bd86b9b2aaf117beb6121d5f49f5794f40e85f92
amacomber <68449783+amacomber@users.noreply.github.com>
2022-08-01 19:18:31 -0600

    Create Enable-TouchID-sudo.sh

---
+++ Actions/Enable-TouchID-sudo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Enable touch id for sudo
+
+sed="/usr/bin/sed"
+
+enable_touchid="auth       sufficient     pam_tid.so"
+
+${sed} -i '' -e "1s/^//p; 1s/^.*/${enable_touchid}/" /etc/pam.d/sudo
+
+exit

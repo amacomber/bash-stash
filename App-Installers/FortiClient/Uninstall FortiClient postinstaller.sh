9b64dec405102b40b4cf173ef60de822b215a288
amacomber <68449783+amacomber@users.noreply.github.com>
2022-08-10 20:17:02 -0600

    update

---
+++ App-Installers/FortiClient/Uninstall FortiClient postinstaller.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Check if FortiClient folder exists
+[ -d "/Applications/FortiClient.app" ] && /Applications/FortiClientUninstaller.app/Contents/Library/LaunchServices/com.fortinet.forticlient.uninstall_helper -v;
+echo "Ran FortiClient Uninstaller."
+
+[ -f "/Applications/FortiClient.app" ] && rm -rf "/Applications/FortiClient.app";
+echo "Removed FortiClient App."
+
+[ -f "/Applications/FortiClientUninstaller.app" ] && rm -rf "/Applications/FortiClientUninstaller.app";
+echo "Removed FortiClient Uninstaller App."
+
+sleep 1
+[ -d "/Library/Application Support/Fortinet/" ] && rm -rf "/Library/Application Support/Fortinet/";
+echo "Removing /Library/Application Support/Fortinet/ folder."
+
+sleep 1
+[ -f "/private/var/root/Library/Preferences/com.fortinet.FortiClientAgent.plist" ] && rm -rf "/private/var/root/Library/Preferences/com.fortinet.FortiClientAgent.plist";
+echo "Removing root plist"
+
+# Clean up temp files
+[ -d "/Library/FortiClient/" ] && rm -rf "/Library/FortiClient";
+echo "Cleaned up installer files."
+
+
+exit 0

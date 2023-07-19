de6d4c5bd53ab986b1e1c8d81687fb609f438fd6
amacomber <amacomber33@gmail.com>
2023-07-18 21:41:36 -0600

    added some scripts

--- Actions/Remove-Admin-Current-User.sh
+++ Actions/Remove-Admin-Current-User.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-admin=$(dscl . -read /Groups/admin GroupMembership | awk '{print $3}')
 currentuser=$(ls -l /dev/console | awk '{ print $3 }')
+admin=$(dscl . -read /Groups/admin GroupMembership | grep -o "$currentuser")
 
 if [[ "$sadmin" == "$currentuser" ]] && /usr/sbin/dseditgroup -o edit -d $currentuser -t user admin && echo "Converted to standard user."
 

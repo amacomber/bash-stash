c271d08764b275ef4f733cceec7ba0e825b70353
Aaron Macomber <amacomber33@gmail.com>
2024-07-09 09:30:56 -0600

    Update ZAProxy.sh

--- App-Installers/ZAProxy.sh
+++ App-Installers/ZAProxy.sh
@@ -1,25 +1,50 @@
 #!/bin/bash
 
-#DMG Download URL
-DownloadUrl=$( curl -s https://api.github.com/repos/zaproxy/zaproxy/releases/latest | grep "browser_download_url.*dmg" | cut -d : -f 2,3 | tr -d '"' | awk '{$1=$1};1' )
+# Set DMG Download URL based on architecture
+arch=$( uname -m )
 
-workingdir="/private/tmp/Installer"
-pkgname="ZAProxy.pkg"
+# Check the architecture and print the appropriate message
+if [ "$arch" = "arm64" ]; then
+    echo "Apple"
+    DownloadUrl=$( curl -s https://api.github.com/repos/zaproxy/zaproxy/releases/latest | grep "browser_download_url.*dmg" | cut -d : -f 2,3 | tr -d '"' | awk '{$1=$1};1' | tail -1 )
+    echo "$DownloadUrl"
+elif [ "$arch" = "x86_64" ]; then
+    echo "Intel"
+    DownloadUrl=$( curl -s https://api.github.com/repos/zaproxy/zaproxy/releases/latest | grep "browser_download_url.*dmg" | cut -d : -f 2,3 | tr -d '"' | awk '{$1=$1};1' | head -1 )
+    echo "$DownloadUrl"
+else
+    echo "Unknown architecture: $arch"
+    echo "This is a bad ending."
+    exit 1 # Bad Ending
+fi
 
-mkdir "$workingdir";
-cd "$workingdir";
-echo "$pwd"
-echo "Installer Directory created"
 
-curl -L -o "$workingdir"/"$pkgname" "$DownloadUrl"
-echo "ZAProxy Downloaded"
+DMG_PATH="/private/tmp/Installer/ZAProxy.dmg"
+MOUNT_POINT="/Volumes/ZAP"
+INSTALLER_DIR="/private/tmp/Installer"
+APPLICATIONS_DIR="/Applications"
+APP_NAME="ZAP.app"
 
-#install the ZAProxy APP from PKG file
-installer -allowUntrusted -pkg "$workingdir"/"$pkgname" -target /;
-echo "ZAProxy Installed"
+# Create the Installer directory
+mkdir -p "$INSTALLER_DIR"
+[ -d "$INSTALLER_DIR" ] && echo "Installer Directory created"
 
-#Remove Installer files
-sudo rm -rf "$workingdir";
-echo "Cleaned up Installer files"
+# Download the DMG
+curl -L -o "$DMG_PATH" "$DownloadUrl"
+[ -f "$DMG_PATH" ] && echo "ZAProxy Downloaded"
+
+# Mount the DMG
+hdiutil attach "$DMG_PATH" -mountpoint "$MOUNT_POINT"
+
+# Copy the app to the Applications folder
+cp -R "$MOUNT_POINT/$APP_NAME" "$APPLICATIONS_DIR"
+[ -f "/Volumes/ZAP.app" ] && echo "ZAProxy Installed"
+
+# Unmount the DMG
+hdiutil detach "$MOUNT_POINT"
+
+# Remove the Installer directory
+rm -rf "$INSTALLER_DIR"
+[ ! -f "$INSTALLER_DIR" ] && echo "Cleaned up Installer files"
 
 exit 0
